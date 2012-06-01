1096c4a8c6d43c0cfb83dcc22417d466c2dc112b
liujun <liujun@netgen.com.cn>
2012-06-01 15:57:42 +0800

    debug

--- urlopened.js
+++ urlopened.js
@@ -6,11 +6,16 @@ var tool = require('./lib/tool').tool;
 var mcli = mysql.createClient(settings.mysql.weibo);
 
 var rq = queue.getQueue('http://'+settings.queue.host+':'+settings.queue.port+'/queue', settings.queue.repost);
-de.on("open-url", function(microBlogId){
-    console.log(micro_blog_id + " url opend");
-    db.insertTask(microBlogId, 'a_stock', function(err, result){
+de.on("open-url", function(ev){
+    if(!ev || !ev.meta || !ev.meta.match(/^\d+$/)){
+        return;
+    }
+    var microBlogId = ev.meta;
+    insertTask(microBlogId, 'a_stock', function(err, result){
         if(err){
-            console.log(err);
+            if(err.number != 1062){
+                console.log(err);
+            }
         }else{
             var task = "mysql://172.16.39.117:3306/weibo?repost_task#" + result.insertId;
             rq.enqueue(task);
@@ -18,10 +23,11 @@ de.on("open-url", function(microBlogId){
     });
 });
 
- var insertTask = function(microBlogId, stockCode, callback){
+var insertTask = function(microBlogId, stockCode, callback){
     var sql = "INSERT INTO repost_task(micro_blog_id, stock_code, in_time) VALUES(?, ?, ?)";
     mcli.query(sql, [microBlogId, stockCode, tool.timestamp()], callback);
 }
+console.log("urlopen listen start at " + tool.getDateString());
 
 
 setTimeout(function(){
