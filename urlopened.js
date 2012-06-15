3c0642ecaf6496abaa3cc7c9f432bf77dc489d2e
liujun <liujun@netgen.com.cn>
2012-06-15 17:18:37 +0800

    debug

--- urlopened.js
+++ urlopened.js
@@ -6,30 +6,39 @@ var tool = require('./lib/tool').tool;
 var mcli = mysql.createClient(settings.mysql.weibo);
 
 var rq = queue.getQueue('http://'+settings.queue.host+':'+settings.queue.port+'/queue', settings.queue.repost);
-de.on("open-url", function(ev){
+
+var run = function(ev){
     if(!ev || !ev.meta || !ev.meta.match(/^\d+$/)){
         return;
     }
     var microBlogId = ev.meta;
-    insertTask(microBlogId, 'a_stock', function(err, result){
-        if(err){
-            if(err.number != 1062){
-                console.log(err);
-            }
-        }else{
-            var task = "mysql://172.16.39.117:3306/weibo?repost_task#" + result.insertId;
-            rq.enqueue(task);
+
+    getMicroBlog(microBlogId, function(err, result){
+        if(err || result.length == 0 || result[0].stock_code == 'a_stock'){
+            return;
         }
+        insertTask(microBlogId, 'a_stock', function(err, result){
+            if(err){
+                if(err.number != 1062){
+                    console.log(err);
+                }
+            }else{
+                var task = "mysql://172.16.39.117:3306/weibo?repost_task#" + result.insertId;
+                rq.enqueue(task);
+            }
+        });
     });
-});
+    
+}
+de.on("open-url", run);
+
+var getMicroBlog = function(id, callback){
+    var sql = "SELECT * FROM micro_blog WHERE id = ?";
+    mcli.query(sql, [id], callback);
+}
 
 var insertTask = function(microBlogId, stockCode, callback){
     var sql = "INSERT INTO repost_task(micro_blog_id, stock_code, in_time) VALUES(?, ?, ?)";
     mcli.query(sql, [microBlogId, stockCode, tool.timestamp()], callback);
 }
 console.log("urlopen listen start at " + tool.getDateString());
-
-
-setTimeout(function(){
-
-}, 1000);
